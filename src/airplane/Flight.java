6cb01722b6ce55a4ea6038fd17e0e848e8191882
cathy.wzx <@LAPTOP-713P3KID>
2019-03-30 10:22:10 +0800

    更新了预定航班信息

--- src/airplane/Flight.java
+++ src/airplane/Flight.java
@@ -6,6 +6,7 @@ public class Flight implements FlightInterface{
 	private int rowLength;//每排座位数
 	private int[] fail= {-1};//返回预订号
 	private Passenger[] passengerList;//预订座位的旅客
+//--------------------初始化航班信息--------------------//
 	public Flight(String FlightName,int rows,int rowLength) throws Exception{
 		if(FlightName==null || FlightName.trim().length()==0 || rows<=0 || rowLength<=0)
 			throw new Exception("Error!");
@@ -18,10 +19,32 @@ public class Flight implements FlightInterface{
 				passengerList[i]=null;//所有座位没有被预订
 		}
 	}
-	@Override
+//--------------------预订航班信息--------------------//
 	public int[] reserve(String[] names) {
-		// TODO 自动生成的方法存根
-		return null;
+		if(names.length>rowLength)//乘客人数大于一排的座位个数则不能安排座位
+			return fail;
+		int i=0,j=0,k=0;
+		boolean flag=false;
+		labelA:for(i=0;i<=row-1;i++) {//遍历row-1排座位
+			for(j=0;j<=rowLength-names.length;j++) {
+				for(k=j;k<=j+names.length-1;k++) {//从第j个位置开始查找一排是否有names.length个相邻的空座位
+					if(passengerList[i*rowLength+k]!=null)
+						break;
+				}
+				if(k>j+names.length-1) {//已找到
+					flag=true;//标记
+					break labelA;//退出整个lablelA循环
+				}
+			}
+		}
+			if(!flag)
+				return fail;
+			int[] bn=new int[names.length];//从第i行的j个位置开始分配座位
+			for(k=j;k<=j+names.length-1;k++) {
+				bn[k-j]=i*rowLength+k+1;//生成预订号
+				passengerList[i*rowLength+k]=new Passenger(names[k-j],i*rowLength+k+1,i,k);//i,排数；k，位置
+			}
+			return bn;
 	}
 	@Override
 	public boolean cancle(int bookingNumber) {
