a1d741ebeff86627de1df7a8e51c63ada14e85d0
cathy.wzx <@LAPTOP-713P3KID>
2019-04-18 22:21:18 +0800

    第二题完成

--- src/regex/Mystring.java
+++ src/regex/Mystring.java
@@ -8,8 +8,12 @@ public class Mystring {
 	    System.out.println("string类型转为stringbuffer类型：\n"+sb);
 	    StringBuffer sbuff = new StringBuffer("My stringbuffer!");
 	    System.out.println("stringbuffer类型转为string类型：\n"+sbuff.toString());
-	    //，测试StringBuffer类的添加、插入、删除、反转操作。
-	    
+	    //测试StringBuffer类的添加、插入、删除、反转操作。
+	  
+	    System.out.println("stringbuffer的插入:"+sb.insert(2," First"));
+	    System.out.println("stringbuffer的删除:"+sb.delete(0,3));
+	    System.out.println("stringbuffer的添加:"+sb.append("添加！！"));
+	    System.out.println("stringbuffer的反转:"+sb.reverse());
 	}
 
 }
