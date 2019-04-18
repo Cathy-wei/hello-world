fd06a7bafb7dfa349620486ae1990620d911f3ec
cathy.wzx <@LAPTOP-713P3KID>
2019-04-19 00:38:21 +0800

    (2)

--- src/regex/Regexxxx.java
+++ src/regex/Regexxxx.java
@@ -13,6 +13,12 @@ public class Regexxxx {
 		Matcher m2=p1.matcher(str2);
 			System.out.println(m1.matches());
 			System.out.println(m2.matches());
+		//（2）将一个字符串中所有的ip地址打印出来
+		String str3="Ip1:198.122.3.99;Ip2:194.132.2.29";
+		Pattern p2=Pattern.compile("(\\d{1,3}\\.){3}(\\d{1,3})");
+		Matcher m3=p2.matcher(str3);
+		while(m3.find())
+		System.out.println(m3.group());
 	}
 
 }
