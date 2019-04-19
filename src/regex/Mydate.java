25a9b666025fb21b914d21df2baa0ce451de321f
cathy.wzx <@LAPTOP-713P3KID>
2019-04-19 11:28:36 +0800

    5.日期类

---
+++ src/regex/Mydate.java
@@ -0,0 +1,27 @@
+package regex;
+import java.time.*;
+import java.time.format.*;
+//		5.日期类。
+public class Mydate {
+
+	public static void main(String[] args) {
+//		使用java8的日期时间包获取当前系统时间，并以各种格式加以显示
+		LocalDateTime time=LocalDateTime.now();
+		System.out.println(time);
+		System.out.println(time.format
+				(DateTimeFormatter.ofLocalizedDateTime
+						(FormatStyle.SHORT,FormatStyle.SHORT)));
+		System.out.println(time.format
+				(DateTimeFormatter.ofLocalizedDateTime
+						(FormatStyle.MEDIUM,FormatStyle.MEDIUM)));
+		System.out.println(time.format
+				(DateTimeFormatter.ofLocalizedDateTime
+						(FormatStyle.FULL,FormatStyle.LONG)));
+//		使用java8的日期时间包获取当前系统时间，并修改这个时间（分别修改年份、月份、天、小时），然后打印结果
+		
+		
+//		从控制台输入一个yyyy-MM-dd（年月日）格式的日期字符串，再将这个日期加上七天，再用FormatStyle.MEDIUM格式输出这个日期。
+		
+	}
+
+}
