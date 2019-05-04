e04c034ccdac6020496be8f149008dd2fa5b3727
cathy.wzx <@LAPTOP-713P3KID>
2019-05-05 01:28:16 +0800

    2

--- src/lambda/MyApple.java
+++ src/lambda/MyApple.java
@@ -17,7 +17,13 @@ class Test {
 public class MyApple {
 
 	public static void main(String[] args) {
-		// TODO 自动生成的方法存根
+		//Test t=new Test();
+		Apple a1=new Apple("green", 13.0);
+		Apple a2=new Apple("red", 10.0);
+		Apple[] apps=new Apple[2];
+		apps[0]=a1;
+		apps[1]=a2;
+		Test.ConsumerApple(apps, (Apple a)->System.out.println("颜色："+a.color+" 重量："+a.weight));
 
 	}
 
