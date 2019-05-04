b40e2f8457e810f0dd8c7ce91d7efc8d0ce63fbe
cathy.wzx <@LAPTOP-713P3KID>
2019-05-05 01:33:46 +0800

    匿名内部类

--- src/lambda/MyApple.java
+++ src/lambda/MyApple.java
@@ -1,4 +1,7 @@
 package lambda;
+
+import java.util.Objects;
+
 class Apple{
 	String color="red";
 	double weight=10.0;
@@ -23,8 +26,18 @@ public class MyApple {
 		Apple[] apps=new Apple[2];
 		apps[0]=a1;
 		apps[1]=a2;
+		//用lambda的形式完成参数Consumer<Apple> c的传递
 		Test.ConsumerApple(apps, (Apple a)->System.out.println("颜色："+a.color+" 重量："+a.weight));
+        //用匿名内部类的形式
+		Test.ConsumerApple(apps,new Consumer<Apple>() {
 
+			@Override
+			public void accept(Apple t) {
+				// TODO 自动生成的方法存根
+				System.out.println("颜色："+t.color+" 重量："+t.weight);
+			}
+			
+		});
 	}
 
 }
